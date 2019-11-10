4294b596d9ce6a21810c0712bbfe2a84f7ae9b8f
davet2001 <davet2001@users.noreply.github.com>
2019-11-10 19:07:06 +0000

    Copy compiled engMQTTClient binary /usr/bin/ Update file ownership after copying

--- service/install.sh
+++ service/install.sh
@@ -3,9 +3,15 @@
 # Set up engmqttclient as a systemd service
 
 echo "Copying files..."
+cp -i ../engMQTTClient /usr/bin/
+chown root:root /usr/bin/engMQTTClient
 cp -i engmqttclient.conf /etc/engmqttclient.conf 
+chown root:root /etc/engmqttclient.conf
 cp -i engMQTTClient_service.sh /usr/bin/engMQTTClient_service.sh
+chown root:root /usr/bin/engMQTTClient_service.sh
 cp -i engmqttclient.service /etc/systemd/system/engmqttclient.service
+chown root:root /etc/systemd/system/engmqttclient.service
+
 echo "Calling systemctl daemon-reload..."
 systemctl daemon-reload
 
